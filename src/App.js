28d7095f676112442d8c61d1f4b1d21d630def2c
amoo34 <a.moizfaisal1234@gmail.com>
2023-02-01 00:18:43 +0500

    add git actionsss

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacthhh
+          Learn Reacthhhs
         </a>
       </header>
     </div>
