97c132ef2a7ea6848c0c7f6abea86e37a60cc01d
amoo34 <a.moizfaisal1234@gmail.com>
2023-02-02 00:34:50 +0500

    kia karo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> as ifghjgjhre 
+         me kia karo
         </p>
         <a
           className="App-link"
